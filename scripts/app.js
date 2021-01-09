a6fc742faf545d3c3f09c744f3675153f22003da
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-01-09 21:52:43 +0100

    added api calls, started UI update and added cloud images

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+
+const updateUI = (data) => {
+
+	// const cityDetails = data.cityDetails;
+	// const weather = data.weather;
+
+	//destructuring properties (an easy way to get properties from an object and then store them in a constant of the same name)
+	const {cityDetails, weather} = data;
+
+	// update the details div template 
+	details.innerHTML = `
+		<h5 class="my-3">${cityDetails.EnglishName}</h5>
+		<div class="my-3">${weather.WeatherText}</div>
+		<div class="display-4 my-4">
+			<span>${weather.Temperature.Metric.Value}</span>
+			<span>&deg;C</span>
+		</div>
+	`; 
+	if(card.classList.contains('d-none')){
+		card.classList.remove('d-none');
+	}
+};
+
+const updateCity = async (city) => { //make a function async if it makes use of other async functions in its body
+
+	const cityDetails = await getCity(city);
+	const weather = await getWeather(cityDetails.Key);
+
+	return { //when you need to return more than one thing, you can return an object
+		//whose properties are those. Now the syntax below is returnin an object alright
+		//but we are using Object Shorthand Notation(which can be used when the property and value of an object have the same name, in which case we remove the property name, including the ':', remaining only the value name)
+		cityDetails, weather };
+};
+
+
+form.addEventListener('submit', e => {
+	//prevent default action
+	e.preventDefault();
+
+	// get city value from input, and remove whitespaces
+	const city = form.city.value.trim();
+	form.reset();
+
+	//update the ui with the new city
+	updateCity(city) 
+	.then(data => updateUI(data))
+	.catch(err => console.log(err));
+});
---
+++ scripts/forecast.js
@@ -0,0 +1,31 @@
+const key = "4hqHyZzHUVhwH5m198H7fVpWAZwUWPj7";  
+//we find information about API endpoints at API references page
+
+
+
+//get weather information
+const getWeather = async (location_id) => {
+	const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+	const query =  `${location_id}?apikey=${key}`;
+	const response = await fetch(base + query);
+	const data = await response.json();
+
+	return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+	const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+	const query = `?apikey=${key}&q=${city}`;
+	const response = await fetch(base + query);
+	const data = await response.json();
+	
+	return data[0];
+};
+
+getCity('manchester').then(data => {
+	return getWeather(data.Key);
+}).then(data => {
+})
+.catch(err => console.log(err));
+
