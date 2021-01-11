4e9b07aac417b7f112d1ce7615fb2805c4e0b52a
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-01-12 00:42:53 +0100

    Add local storage functionality

---
+++ scripts/local.js
@@ -0,0 +1,12 @@
+// const todos = [
+// 	{text: 'play mariokart', author: 'shaun'},
+// 	{text: 'buy some milk', author: 'mario'},
+// 	{text: 'buy some bread', author: 'luigi'}
+// ];
+// //turns it into a JSON string which can be stored in the browser's local storage(only stores strings)
+// localStorage.setItem('todos', JSON.stringify(todos));
+// //JSON.stringify() will convert an array of objects, into a JSON string format(properties will have double qoutes)
+
+// const stored = localStorage.getItem('todos');
+// console.log(JSON.parse(stored));
+// //JSON.parse() will take a JSON string and parse it into its equivalent of an array of object
